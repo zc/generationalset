ffe762f6b3950453b41a16cd249ee84e4cc1363f
Jim Fulton <jim@jimfulton.info>
2017-06-20 16:31:07 -0400

    Added meta data

--- setup.py
+++ setup.py
@@ -47,4 +47,17 @@ setup(
     extras_require = extras_require,
     tests_require = extras_require['test'],
     test_suite = name+'.tests.test_suite',
+    keywords = ['real-time UI', 'ZODB'],
+    classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+       ],
     )
